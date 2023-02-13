50ae58b93f6a3527229f03230b7748fdc59f81b1
Lucas Corrêa <65604927+correa0105@users.noreply.github.com>
2023-02-12 23:49:51 -0300

    Finalizando seção 4

--- Seção4/templateEngine/index.js
+++ Seção4/templateEngine/index.js
@@ -1,13 +1,75 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
+const path = require('path')
+const ejs = require('ejs')
 
 const app = express()
 
-app.engine('handlebars', exphbs.engine())
-app.set('view engine', 'handlebars')
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static('public'))
+
+app.get('/dashboard', (req, res) => {
+
+    const items = ["Item a", "Item b", "Item c", "Item d"]
+
+    res.render('dashboard', { items })
+})
+
+app.get('/blogpost', (req, res) => {
+
+    const post = {
+        title: 'Aprendendo NodeJS',
+        category: 'JavaScript',
+        body: 'Este artigo vai te ajudar a aprender NodeJS',
+        comments: 4
+    }
+
+    res.render('blogpost', { post })
+})
+
+app.get('/blog', (req, res) => {
+
+    const posts = [{
+        title: 'Aprendendo NodeJS',
+        category: 'JavaScript',
+        body: 'Este artigo vai te ajudar a aprender NodeJS',
+        comments: 4
+    },
+    {
+        title: 'Aprendendo JavaScript',
+        category: 'JavaScript',
+        body: 'Este artigo vai te ajudar a aprender JavaScript',
+        comments: 5
+    },
+    {
+        title: 'Aprendendo Python',
+        category: 'Python',
+        body: 'Este artigo vai te ajudar a aprender Python',
+        comments: 1
+    },
+    {
+        title: 'Aprendendo EJS',
+        category: 'EJS',
+        body: 'Este artigo vai te ajudar a aprender EJS',
+        comments: 3
+    }
+    ]
+
+    res.render('blog', { posts })
+})
 
 app.get('/', (req, res) => {
-    res.render('home', { layout: false })
+
+    const user = {
+        name: 'Lucas',
+        lastname: 'Corrêa',
+        age: 23
+    }
+
+    const auth = true
+
+    res.render('home', { user, auth })
 })
 
 app.listen(3000, () => {
